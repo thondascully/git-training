e563fd598b827629a5a43fea2db763be93b4ea07
ccsaposs <ccsaposs+github@gmail.com>
2019-01-01 19:37:23 -1000

    Add SPR.py
    Calculates SPRs (Scout Precision Ranking).

---
+++ SPR.py
@@ -0,0 +1,20 @@
+"""Calculates SPRs (Scout Precision Ranking).
+
+Called by server.py"""
+# External imports
+import random
+# No internal imports
+
+SPRs = {
+    "Aakash": 12.6,
+    "Carl": 1.2,
+    "David": 0.1,
+    "Kyle": 0.7,
+    "Sam C": 0.8,
+}
+
+def calculateSPRs():
+    for scout, spr in SPRs.items():
+        if scout == 'Aakash':
+            SPRs[scout] = spr*random.randint(1,5)
+        print(f'{scout} has an SPR of {SPRs[scout]}')

d27b1592eaf76144d443ae210121858e7114bc92
ccsaposs <ccsaposs+github@gmail.com>
2019-01-01 19:36:54 -1000

    Add server.py
    Main server file.  Calculates SPRs every 15 seconds.

---
+++ server.py
@@ -0,0 +1,9 @@
+"""Main server file.  Calculates SPRs every 15 seconds."""
+# External imports
+import time
+# Internal imports
+import SPR
+
+while True:
+    SPR.calculateSPRs()
+    time.sleep(5)
